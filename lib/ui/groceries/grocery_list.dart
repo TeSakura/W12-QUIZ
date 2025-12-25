88858b994328331a04484a275e26071426819164
TeSakura <149454188+TeSakura@users.noreply.github.com>
2025-12-25 14:03:30 +0700

    Update grocery_list.dart

--- lib/ui/groceries/grocery_list.dart
+++ lib/ui/groceries/grocery_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import '../../data/mock_grocery_repository.dart';
 import '../../models/grocery.dart';
+import 'grocery_form.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -11,41 +11,59 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void onCreate() {
-    // TODO-4 - Navigate to the form screen using the Navigator push
+  late List<Grocery> _items;
+
+  @override
+  void initState() {
+    super.initState();
+    _items = List.from(dummyGroceryItems);
+  }
+
+  void onCreate() async {
+    // Navigate to the form and await a new Grocery returned by Navigator.pop
+    final result = await Navigator.of(
+      context,
+    ).push<Grocery>(MaterialPageRoute(builder: (ctx) => const NewItem()));
+
+    if (result != null) {
+      setState(() {
+        _items.add(result);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(child: Text('No items added yet.'));
 
-    if (dummyGroceryItems.isNotEmpty) {
-      //  Display groceries with an Item builder and  LIst Tile
+    if (_items.isNotEmpty) {
+      // Display groceries with an Item builder and ListTile
       content = ListView.builder(
-        itemCount: dummyGroceryItems.length,
-        itemBuilder: (context, index) => GroceryItem(grocery: dummyGroceryItems[index],),
+        itemCount: _items.length,
+        itemBuilder: (ctx, index) => GroceryTile(grocery: _items[index]),
       );
     }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
-        actions: [IconButton(onPressed: () => {}, icon: const Icon(Icons.add))],
+        actions: [IconButton(onPressed: onCreate, icon: const Icon(Icons.add))],
       ),
       body: content,
     );
   }
 }
 
-class GroceryItem extends StatelessWidget {
-  const GroceryItem({super.key, required this.grocery});
+class GroceryTile extends StatelessWidget {
+  const GroceryTile({super.key, required this.grocery});
 
   final Grocery grocery;
 
   @override
   Widget build(BuildContext context) {
+    // TODO-2 - Display groceries with an Item builder and  LIst Tile
     return ListTile(
-      leading: Container(color: grocery.category.color, width: 15, height: 15,),
+      leading: Container(color: grocery.category.color, width: 15, height: 15),
       title: Text(grocery.name),
       trailing: Text(grocery.quantity.toString()),
     );
